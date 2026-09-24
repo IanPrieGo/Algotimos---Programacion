02580735d549e5880f0860ff2f7ac8ae493c3f10
Ian Priet-Gom <prieto.ipg@gmail.com>
2026-09-24 07:43:22 -0600

    Clase 24 Sep, ejercicio 1, actividad 9

---
+++ Parcial2/Clase24SEP/E1A9.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+
+int main(){
+
+    float input1 = 0.0, input2 = 0.0, res = 0.0;
+    std::string operation;
+
+    std::cout << "Ingresa un Numero:\n";
+    std::cin >> input1;
+
+
+    std::cout << "Ingresa otro Numero:\n";
+    std::cin >> input2;
+
+    std::cout << "\nResultado:\n";
+    if(input1 < input2){
+        res = input1 + input2;
+        operation = "+";
+    } else {
+        if (input1 > input2){
+            res = input1 - input2;
+            operation = "-";
+        } else {
+            res = input1 * input2;
+            operation = "*";
+        }
+    }
+
+
+    std::cout << input1 << " " << operation << " " << input2 << " = " << res;
+
+
+    return 0;
+}
